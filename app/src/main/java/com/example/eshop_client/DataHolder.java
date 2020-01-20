176eb10c578182eb39a212b4502f9b80ae840c38
karimmady <karimmady5@hotmail.com>
2020-01-21 00:18:56 +0200

    dashboard

---
+++ app/src/main/java/com/example/eshop_client/DataHolder.java
@@ -0,0 +1,17 @@
+package com.example.eshop_client;
+
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+
+public class DataHolder {
+    private String email;
+    List<NameValuePair> postInfo;
+    public String getEmail() {return email;}
+    public void setEmail(String mail) {this.email = mail;}
+    public void setPostInfo(List<NameValuePair> nameValuePairs){postInfo = nameValuePairs;}
+    public List<NameValuePair>getPostInfo(){return postInfo;}
+
+    private static final DataHolder holder = new DataHolder();
+    public static DataHolder getInstance() {return holder;}
+}
