b87bfd9ca01d38760d962d0e268d8f7bf38bef70
Marwan Hazem <marwanhazem@Marwans-MacBook-Pro-2.local>
2020-01-21 00:04:50 +0200

    latestmarwan

--- app/src/main/java/com/example/eshop_client/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/eshop_client/ui/home/HomeFragment.java
@@ -49,7 +49,7 @@ public class HomeFragment extends Fragment {
 //        });
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         try {
-            network.execute("http://10.0.2.2:3000/getbrands").get();
+            network.execute("http://192.168.1.41:3000/getbrands").get();
             brands = network.jsono;
             brandsArray = (JSONArray) brands.get("data");
             System.out.println(brandsArray);
@@ -90,64 +90,3 @@ public class HomeFragment extends Fragment {
     }
 }
 
-class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
-
-    ArrayList<String> brandNames;
-    ArrayList<Integer> brandImages;
-    Context context;
-
-    public CustomAdapter(Context context, ArrayList<String> brandNames, ArrayList<Integer> brandImages) {
-        this.context = context;
-
-        this.brandNames = brandNames;
-        this.brandImages = brandImages;
-    }
-
-    @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // infalte the item Layout
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
-        return vh;
-    }
-
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position) {
-        // set the data in items
-        holder.name.setText(brandNames.get(position));
-        holder.image.setImageResource(brandImages.get(position));
-        // implement setOnClickListener event on item view.
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // open another activity on item click
-                Intent intent = new Intent(context, Brands.class);
-                intent.putExtra("image", brandImages.get(position)); // put image data in Intent
-                context.startActivity(intent); // start Intent
-            }
-        });
-
-    }
-
-
-    @Override
-    public int getItemCount() {
-        return brandNames.size();
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        // init the item view's
-        TextView name;
-        ImageView image;
-
-        public MyViewHolder(View itemView) {
-            super(itemView);
-
-            // get the reference of item view's
-            name = (TextView) itemView.findViewById(R.id.name);
-            image = (ImageView) itemView.findViewById(R.id.image);
-
-        }
-    }
-}
