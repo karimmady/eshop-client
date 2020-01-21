d1d3d118352eceb529c7441f73a6b310782c9ec9
mahmoudsultan1 <mahmoudsultan@aucegypt.edu>
2020-01-21 21:13:37 +0200

    brandslayout,home,noti

--- app/src/main/java/com/example/eshop_client/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/eshop_client/ui/home/HomeFragment.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.eshop_client.CustomAdapter;
 
 import com.example.eshop_client.DataHolder;
 import com.example.eshop_client.Network;
@@ -38,12 +37,12 @@ public class HomeFragment extends Fragment {
     JSONObject brands;
     JSONArray brandsArray;
     ArrayList<String> brandNames = new ArrayList<>();//(Arrays.asList("brand 1", "brand 2", "brand 3", "brand 4", "brand 5", "brand 6", "brand 7","brand 8", "brand 9", "brand 10", "brand 11", "brand 12", "brand 13", "brand 14"));
-    ArrayList<Integer> brandImages = new ArrayList<>();//(Arrays.asList(R.drawable.Bisho, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan,R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan, R.drawable.tahan));
+    ArrayList<Integer> brandImages = new ArrayList<>(Arrays.asList(R.drawable.zara, R.drawable.zara, R.drawable.nike));
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         try {
-            network.execute("http://192.168.1.41:3000/getbrands").get();
+            network.execute("http://172.20.10.2:3000/getbrands").get();
             brands = network.jsono;
             brandsArray = (JSONArray) brands.get("data");
             System.out.println(brandsArray);
@@ -76,6 +75,70 @@ public class HomeFragment extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
             return -1;
+        }
+    }
+}
+
+    class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    ArrayList<String> brandNames;
+    ArrayList<Integer> brandImages;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList<String> brandNames, ArrayList<Integer> brandImages) {
+        this.context = context;
+
+        this.brandNames = brandNames;
+        this.brandImages = brandImages;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.name.setText(brandNames.get(position));
+        holder.image.setImageResource(brandImages.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open another activity on item click
+                Intent i= new Intent(context, Brands.class);
+                Bundle X=new Bundle();
+                X.putString("Bname",brandNames.get(position));
+                i.putExtras(X);
+                context.startActivity(i);
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return brandNames.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        // init the item view's
+        TextView name;
+        ImageView image;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            // get the reference of item view's
+            name = (TextView) itemView.findViewById(R.id.name);
+            image = (ImageView) itemView.findViewById(R.id.image);
+
         }
     }
 }
